d570b6a2ce0d1daa928aa6ff7feef122b1093cac
Eric Lee <ayrocklee@gmail.com>
2018-10-02 21:06:52 -0500

    Updates

---
+++ precache-manifest.0c9ddc2feced886e545318149c78e2f3.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "591f15a0e55e5eac2502",
+    "url": "/murder-mystery/static/js/runtime~main.591f15a0.js"
+  },
+  {
+    "revision": "e16daacff53477e64869",
+    "url": "/murder-mystery/static/js/main.e16daacf.chunk.js"
+  },
+  {
+    "revision": "7459e2f11354ac003180",
+    "url": "/murder-mystery/static/js/1.7459e2f1.chunk.js"
+  },
+  {
+    "revision": "e16daacff53477e64869",
+    "url": "/murder-mystery/static/css/main.61d02bff.chunk.css"
+  },
+  {
+    "revision": "53fc08bc34ba9e493cbe1981262c0083",
+    "url": "/murder-mystery/index.html"
+  }
+];
