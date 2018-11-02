54cb0a87122606c271292f2753f9e86fbf843904
Eric Lee <ayrocklee@gmail.com>
2018-11-02 03:03:59 -0500

    Updates

---
+++ precache-manifest.b77fe145cb1b911584ed2f895aff3a3f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "d91f00181d04e3ce28e0914a35029513",
+    "url": "/murder-mystery/static/media/bg.d91f0018.png"
+  },
+  {
+    "revision": "591f15a0e55e5eac2502",
+    "url": "/murder-mystery/static/js/runtime~main.591f15a0.js"
+  },
+  {
+    "revision": "ec2210f54cceba2bbba9",
+    "url": "/murder-mystery/static/js/main.ec2210f5.chunk.js"
+  },
+  {
+    "revision": "692b03d13cadcf6a70e1",
+    "url": "/murder-mystery/static/js/1.692b03d1.chunk.js"
+  },
+  {
+    "revision": "ec2210f54cceba2bbba9",
+    "url": "/murder-mystery/static/css/main.cc0451aa.chunk.css"
+  },
+  {
+    "revision": "b2aee33a75344a6d0c5ca0ec1891a4e1",
+    "url": "/murder-mystery/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -11,10 +11,10 @@
  * See https://goo.gl/2aRDsh
  */
 
-importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.2/workbox-sw.js");
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/murder-mystery/precache-manifest.b3f86ecbe538938d8eeb4dba6fd121c4.js"
+  "/murder-mystery/precache-manifest.b77fe145cb1b911584ed2f895aff3a3f.js"
 );
 
 workbox.clientsClaim();
